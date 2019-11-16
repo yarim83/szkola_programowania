38ca8fec07168f5cbfab12c9f490288fd7510be2
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-11-16 21:36:24 +0100

    Create SolutionAssign

--- src/pl/coderslab/program/adm/SolutionAssign.java
+++ src/pl/coderslab/program/adm/SolutionAssign.java
@@ -2,12 +2,18 @@ package pl.coderslab.program.adm;
 
 import pl.coderslab.dao.SolutionDao;
 import pl.coderslab.dao.UserDao;
+import pl.coderslab.model.Solution;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class SolutionAssign {
 
+    /**
+     * This method is used to print SolutionAssign menu.
+     *
+     * @return Nothing.
+     */
     public void userMenu() {
 
         final String exit = "quit";
@@ -18,14 +24,6 @@ public class SolutionAssign {
             printMenu();
             programState = scanner.nextLine();
             switch (programState) {
-                case "testy":
-
-                    UserDao userDao = new UserDao();
-
-                    SolutionDao solutionDao = new SolutionDao();
-                    System.out.println(Arrays.toString(solutionDao.findAllByUserId(1)));
-
-                    break;
                 case "add":
                     addSolution();
                     break;
@@ -45,34 +43,52 @@ public class SolutionAssign {
     }
 
     /**
-     * This method is used to add two integers. This is
-     * a the simplest form of a class method, just to
-     * show the usage of various javadoc Tags.
+     * This method is used to print option menu.
      *
      * @return Nothing.
      */
     private static void printMenu() {
 
         System.out.println("Chose option:");
-        System.out.println("add - add exercise");
-        System.out.println("edit - edit exercise");
+        System.out.println("add - add solution");
+        System.out.println("view - view solution");
         System.out.println("quit = main menu");
     }
 
     /**
-     * This method is used to add two integers. This is
-     * a the simplest form of a class method, just to
-     * show the usage of various javadoc Tags.
+     * This method is used to add specify solution to user.
+     *
      * @return Nothing.
      */
-    private static void addSolution(){
-        new UserDao().findAll();
+    private static void addSolution() {
+        UserDao userDao = new UserDao();
+        System.out.println(Arrays.toString(userDao.findAll()));
+
+        int userId = 0, solutionId = 0;
+        SolutionDao solutionDao = new SolutionDao();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Point User ID to add Solutions:");
+
+        userId = scanner.nextInt();
+        userDao.read(userId);
+        System.out.println(Arrays.toString(solutionDao.findAll()));
+
+        System.out.println("Point Solution ID to add to User:");
+        solutionId = scanner.nextInt();
+
+        Solution solution = new Solution();
+        try {
+            solution = solutionDao.read(solutionId);
+            solution.setUsers_id(userId);
+            solutionDao.update(solution);
+        } catch (NullPointerException ex) {
+            System.out.println("Wrong solution ID");
+        }
     }
 
     /**
-     * This method is used to add two integers. This is
-     * a the simplest form of a class method, just to
-     * show the usage of various javadoc Tags.
+     * This method is used to view solution assign to specify User ID.
      *
      * @return Nothing.
      */
